2400a41565786c312cfcf3937417675487d873fc
zhongj <featherfly@foxmail.com>
2022-10-25 18:50:02 +0800

    chore: crc

--- xmake.lua
+++ xmake.lua
@@ -12,6 +12,7 @@ add_rules("mode.debug", "mode.release")
 target("c_crc")
     -- make as a static/shared library
     -- set_kind("${kind}")
+    set_kind("static")
 
     add_includedirs("include")
     add_files("src/*.c")
